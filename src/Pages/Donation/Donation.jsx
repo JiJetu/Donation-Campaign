2453051f4ec4b37fa791238dc4f317f8f5acb667
JiJetu <jijetu2@gmail.com>
2023-09-26 18:09:20 +0600

    donation card added in donation router

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import DonationCard from "./DonationCard";
+
 const Donation = () => {
+    const [donation, setDonation] = useState([]);
+    const [noDataFound, setNoDataFound] = useState(false);
+
+    useEffect(() => {
+        const donationItem = JSON.parse(localStorage.getItem("donation"));
+        if (donationItem) {
+            setDonation(donationItem);
+        }
+        else {
+            setNoDataFound("No data found")
+        }
+    }, [])
+
     return (
         <div>
-            
+            {
+                noDataFound ? <p className="h-[80vh] flex justify-center items-center">{noDataFound}</p> : <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-20">
+                    {
+                        donation.map(donationCard => <DonationCard key={donationCard.id} donationCard={donationCard}></DonationCard>)
+                    }
+                </div>
+            }
         </div>
     );
 };
---
+++ src/Pages/Donation/DonationCard.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types"
+
+const DonationCard = ({ donationCard }) => {
+    const { id, image, category, title, card_bg, category_bg, text_color } = donationCard || {};
+    return (
+        <div style={{backgroundColor: card_bg}} className="h-[200px] w-[630px] flex items-center gap-6">
+            <img className="w-[220px] h-full" src={image} alt={title} />
+            <div>
+                <button style={{ backgroundColor: category_bg, color: text_color, padding: '4px 10px', fontSize: "14px", fontWeight: "500", borderRadius: "4px" }}>{category}</button>
+                <h3 className='my-2 text-2xl font-semibold'>{title}</h3>
+                <p style={{color: text_color, margin:"8px 0px"}}>$290.00</p>
+                <button className="text-white" style={{ backgroundColor: text_color, padding: '9px 16px', fontSize: "16px", fontWeight:"700" }}>View Details</button>
+            </div>
+        </div>
+    );
+};
+
+DonationCard.propTypes = {
+    donationCard: PropTypes.object
+}
+
+export default DonationCard;
